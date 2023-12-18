08218668c24b76656744c894ad07430b76f71121
Travis Snapp <traviswsnapp@outlook.com>
2023-12-17 22:03:19 -0500

    get request on projects completed

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,10 @@
 // build your `Project` model here
+const db = require("../../data/dbConfig");
+
+function getAllProjects() {
+  return db("projects");
+}
+
+module.exports = {
+  getAllProjects,
+};
--- api/project/router.js
+++ api/project/router.js
@@ -1,8 +1,18 @@
 // build your `/api/projects` router here
 const router = require("express").Router();
+const Projects = require("./model");
 
-router.get("/", (req, res, next) => {
-  return res.status(200).send("got it project connected");
+router.get("/", async (req, res, next) => {
+  try {
+    const projects = await Projects.getAllProjects();
+
+    if (projects) {
+      res.status(200).json(projects);
+    } else {
+      next();
+    }
+  } catch (err) {
+    next(err);
+  }
 });
-
 module.exports = router;
